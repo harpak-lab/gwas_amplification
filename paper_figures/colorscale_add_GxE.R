7554f9248792cd7cc9162891a2690e0d76450934
Eric Weine <ericweine@MED40857.local>
2022-12-26 13:57:35 -0600

    updated simulation numbers

--- paper_figures/colorscale_add_GxE.R
+++ paper_figures/colorscale_add_GxE.R
@@ -15,16 +15,38 @@ maf_simulator <- function(n) {
   
 }
 
-perc_amp_vec <- c()
-amp_size_vec <- c()
-additive_corr_vec <- c()
-GxE_corr_vec <- c()
+Sigma_init <- list()
+Sigma_init[[1]] <- matrix(data = c(1, 1, 1, 1), ncol = 2)
 
-for (perc_amp in seq(0, 1, length.out = 25)) {
+no_amp_sim <- gamp::simulate_pgs_corr_fast_par(
+  n_e0_train = 2500,
+  n_e1_train = 2500,
+  n_e0_test = 2500,
+  n_e1_test = 2500,
+  n_snps = 10000,
+  maf_simulator,
+  e0_h2 = .4,
+  e1_h2 = .4,
+  Sigma = Sigma_init,
+  pi = c(1),
+  num_sims = 128,
+  s = .8,
+  pval_thresh_additive = 5e-3,
+  pval_thresh_GxE = .07,
+  beta_methods = c("additive", "GxE"),
+  selection_methods = c("additive")
+)
+
+perc_amp_vec <- c(seq(0, 1, length.out = 50), rep(0, 50))
+amp_size_vec <- c(rep(1, 50), seq(1, 3, length.out = 50))
+additive_corr_vec <- c(rep(no_amp_sim$corr[1], 100))
+GxE_corr_vec <- c(rep(no_amp_sim$corr[2], 100))
+
+for (perc_amp in seq(0, 1, length.out = 50)[-1]) {
   
   print(glue::glue("Working on {perc_amp} amplification."))
   
-  for (amp in seq(1, 3, length.out = 25)) {
+  for (amp in seq(1, 3, length.out = 50)[-1]) {
     
     Sigma <- list()
     Sigma[[1]] <- matrix(data = c(1, 1, 1, 1), ncol = 2)
@@ -42,7 +64,7 @@ for (perc_amp in seq(0, 1, length.out = 25)) {
       e1_h2 = .4,
       Sigma = Sigma,
       pi = pi,
-      num_sims = 36,
+      num_sims = 128,
       s = .8,
       pval_thresh_additive = 5e-3,
       pval_thresh_GxE = .07,
@@ -66,16 +88,7 @@ sim_res_df <- data.frame(
   GxE_corr = GxE_corr_vec
 )
 
-#readr::write_csv(sim_res_df, "./colorscale_add_vs_GxE.csv")
-sim_res_df <- readr::read_csv(
-  "./colorscale_add_vs_GxE.csv"
-)
-
 sim_res_df <- sim_res_df %>%
   mutate(additive_improvement = (additive_corr - GxE_corr) / GxE_corr)
 
-ggplot(data = sim_res_df, aes(x = perc_amp, y = amp, fill = additive_improvement)) +
-  geom_tile() +
-  scale_fill_gradient2()
-
-
+readr::write_csv(sim_res_df, "./colorscale_add_vs_GxE.csv")
